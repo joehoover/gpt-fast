f3bbe9febab60a6030cc4059af347e93727241f9
joe <joe@replicate.com>
2023-12-27 03:12:51 +0000

    Add hf_token input to prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,1 +1,2 @@
-python scripts/download.py --repo_id $1 && python scripts/convert_hf_checkpoint.py --checkpoint_dir checkpoints/$1 && python quantize.py --checkpoint_path checkpoints/$1/model.pth --mode int8
+hf_token=${2:-None}
+python scripts/download.py --repo_id $1 --hf_token $hf_token && python scripts/convert_hf_checkpoint.py --checkpoint_dir checkpoints/$1 && python quantize.py --checkpoint_path checkpoints/$1/model.pth --mode int8
